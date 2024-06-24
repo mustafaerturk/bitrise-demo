21433155bd651444fb034954268ffb6f3719b24e
mustafaerturk <mustafa.erturk@halkbank.com.tr>
2024-06-24 16:34:07 +0300

    Implement dummy unit-test

---
+++ app/src/test/java/com/bitrise/demo/MainViewModelTest.kt
@@ -0,0 +1,34 @@
+package com.bitrise.demo
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import com.bitrise.demo.domain.MainViewModel
+import kotlinx.coroutines.test.runTest
+import org.junit.Assert
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+
+class MainViewModelTest {
+
+    @get:Rule
+    val instantTaskExecutorRule = InstantTaskExecutorRule()
+
+    @get:Rule
+    val mainDispatcherRule: MainDispatcherRule = MainDispatcherRule()
+
+    private lateinit var viewModel: MainViewModel
+
+    @Before
+    fun setUp() {
+        viewModel = MainViewModel()
+    }
+
+    @Test
+    fun `when fetch clicked check state`() = runTest {
+        viewModel.fetch()
+
+        val result = viewModel.titleList.value
+
+        Assert.assertTrue(result.isEmpty())
+    }
+}
